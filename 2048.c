4a45bda2635e329a580eb0e169d101c7fd2b5884
devw45 <devin.wingfield45@gmail.com>
2024-02-21 13:00:05 -0600

    original code

--- 2048.c
+++ 2048.c
@@ -9,7 +9,8 @@
 #define RIGHT 'd'
 #define CLEAR printf("\033[H\033[J")
 
-enum Moves {
+enum Moves
+{
     STOP,
     SLIDE,
     MERGE,
@@ -24,13 +25,14 @@ void printLogo();
 
 int board[4][4] = {BLANK};
 
-
-int main (void) {
+int main(void)
+{
     char direction;
     srand(time(NULL));
     placeNewTile();
     placeNewTile();
-    while (1) {
+    while (1)
+    {
         printLogo();
         printBoard();
         direction = getDirection();
@@ -38,30 +40,30 @@ int main (void) {
         printf("direction: %c\n", direction);
         // CLEAR;
     }
-    
-    
-    
-    
-    
-    
+
     return 0;
 }
 
-
-void placeNewTile() {
+void placeNewTile()
+{
     // places a new tile on one of the empty tiles
     int tileVal = BLANK;
     int emptyTiles[16][2];
-    
-    //generate new tile value of "2" (90% chance) or "4" (10%  chance)
-    if (rand() % 10 == 9) tileVal = 4;
-    else tileVal = 2;
 
-    //gather all empty tiles
+    // generate new tile value of "2" (90% chance) or "4" (10%  chance)
+    if (rand() % 10 == 9)
+        tileVal = 4;
+    else
+        tileVal = 2;
+
+    // gather all empty tiles
     int n = 0;
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            if (board[i][j] == BLANK) {
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (board[i][j] == BLANK)
+            {
                 emptyTiles[n][0] = i;
                 emptyTiles[n][1] = j;
                 n++;
@@ -69,12 +71,13 @@ void placeNewTile() {
         }
     }
 
-    //pick a random empty tile and give it the generated value
+    // pick a random empty tile and give it the generated value
     int tilePosition = rand() % n;
     board[emptyTiles[tilePosition][0]][emptyTiles[tilePosition][1]] = tileVal;
 }
 
-void printBoard() {
+void printBoard()
+{
     printf("            -----+----+----+-----\n");
     for (int i = 0; i < 4; i++)
     {
@@ -87,25 +90,27 @@ void printBoard() {
     }
 }
 
-char getDirection() {
+char getDirection()
+{
     char direction;
     printf("\n             Slide Direction: ");
-    scanf(" %c", &direction); //space so it can read the value
+    scanf(" %c", &direction); // space so it can read the value
     return direction;
 }
 
-enum Moves moveTile(int row, int column, char direction) { 
-    //attempt to move a single tile and return its move type
+enum Moves moveTile(int row, int column, char direction)
+{
+    // attempt to move a single tile and return its move type
     int tileVal = board[row][column];
     int nextRow = row;
     int nextColumn = column;
-    int nextTileVal = board[row][column]; //JUST FOR ME
 
     // if attempting to slide a blank tile, stop
     if (tileVal == BLANK)
         return STOP;
 
-    switch (direction) {
+    switch (direction)
+    {
     case UP:
         nextRow--;
         break;
@@ -122,48 +127,62 @@ enum Moves moveTile(int row, int column, char direction) {
         printf("\nError: Cannot read keyboard input!\n");
         break;
     }
+    int nextTileVal = board[nextRow][nextColumn];
 
-    //if the tile is attempting to slide of the board, stop
-    if (nextRow >= 0 || nextRow <= 3 || nextColumn >= 0 || nextColumn <= 3) {
-        nextTileVal = board[nextRow][nextColumn]; // WORKS BETTER BECAUSE IT CHECKS THE INITIALIZATION OF NEXTTILEVAL
+    // if the tile is attempting to slide of the board, stop
+    if (nextRow < 0 || nextRow > 3 || nextColumn < 0 || nextColumn > 3)
+    {
+        printf("out of bounds\n");
+        return STOP;
     }
 
-    //if the next tile is blank, slide current tile to next spot
-    else if (nextTileVal == BLANK) {
-        board[nextRow][nextColumn] = board[row][column]; //IF THERE IS A VAL > 0 IN board[row][column]
+    // if the next tile is blank, slide current tile to next spot
+    else if (nextTileVal == BLANK)
+    {
+        printf("changed to next tile\n");
+        board[nextRow][nextColumn] = board[row][column]; // IF THERE IS A VAL > 0 IN board[row][column]
         board[row][column] = BLANK;
     }
 
-    //if the next tile is not blank and is the same value as the current tile, merge the tiles
-    else if (nextTileVal == tileVal) {
-        board[nextRow][nextColumn] = tileVal << 1; //left shift because it's cooler than * 2
+    // if the next tile is not blank and is the same value as the current tile, merge the tiles
+    else if (nextTileVal == tileVal)
+    {
+        printf("merged tiles\n");
+        board[nextRow][nextColumn] = tileVal << 1; // left shift because it's cooler than * 2
         board[row][column] = BLANK;
     } //
-    
-    //if the tiles are both occupied and do not match, stop
-    else if (nextTileVal != BLANK && nextTileVal != tileVal) return STOP;// THIS LOGIC SEEMS LIKE A PROBLEM, WHAT IF THE NEXT TILE CAN MOVE?
-    
-    //if this happens I missed something
-    else return STOP; // CHNAGED SO THE FUNCTION STOPS IF NOT WITHIN DOMAINS
+
+    // if the tiles are both occupied and do not match, stop
+    else if (nextTileVal != BLANK && nextTileVal != tileVal)
+    {
+        printf("stopped due to val in next tile\n");
+        return STOP;
+    }
+    // if this happens I missed something
+    else
+        return STOP;
 }
 
-int mapTile(int row, int column) {
-    //map a tile's coordinates to a single index number
+int mapTile(int row, int column)
+{
+    // map a tile's coordinates to a single index number
     return (row << 2) | column;
 
- /* This works because the highest row or column value is 3, which is two bits in binary.
-    By left-shifting two bits, there is no overlap in the column and row values,
-    which makes the bitwise OR is just the sum of the two values, 
-    which will be unique for every coordinate. */
+    /* This works because the highest row or column value is 3, which is two bits in binary.
+       By left-shifting two bits, there is no overlap in the column and row values,
+       which makes the bitwise OR is just the sum of the two values,
+       which will be unique for every coordinate. */
 }
 
-void moveBoard(char direction) {
-    //move every tile on the board in a direction
-    int rowStart=0, columnStart=0, rowEnd=3, columnEnd=3, rowCounter=1, columnCounter=1; //SET TO SPECIFIC VALUES BC SOME WERENT IF GIVEN A CERTAIN DIRECTION
-    
-    switch (direction) {
+void moveBoard(char direction)
+{
+    // move every tile on the board in a direction
+    int rowStart = 0, columnStart = 0, rowEnd = 3, columnEnd = 3, rowCounter = 1, columnCounter = 1; // SET TO SPECIFIC VALUES BC SOME WERENT IF GIVEN A CERTAIN DIRECTION
+
+    switch (direction)
+    {
     case UP:
-        rowStart = 1; 
+        rowStart = 1;
         rowCounter = 1;
         rowEnd = 4;
         break;
@@ -185,28 +204,31 @@ void moveBoard(char direction) {
     default:
         break;
     }
-    
+
     int mergedTiles[] = {0};
     int tileIndex;
     enum Moves moveType;
 
-    for (int k = 0; k < 3; k++) { // tiles can only move up to three times in any direction
-        for (int i = rowStart; i != rowEnd; i += rowCounter) { 
-            for (int j = columnStart; j != columnEnd; j += columnCounter) { 
+    for (int k = 0; k < 3; k++)
+    { // tiles can only move up to three times in any direction
+        for (int i = rowStart; i != rowEnd; i += rowCounter)
+        {
+            for (int j = columnStart; j != columnEnd; j += columnCounter)
+            {
                 tileIndex = mapTile(i, j);
                 moveType = moveTile(i, j, direction);
                 // printf("j: %d\n", j);
                 // if (moveType == MERGE) {
                 //     mergedTiles[tileIndex] = 1;
                 // }
-
             }
             // printf("i: %d\n", i);
         }
     }
 }
 
-void printLogo() {
+void printLogo()
+{
     puts("\n  _______  ________  ___   ___  ________     ");
     puts(" /  ___  \\|\\   __  \\|\\  \\ |\\  \\|\\   __  \\    ");
     puts("/__/|_/  /\\ \\  \\|\\  \\ \\  \\\\_\\  \\ \\  \\|\\  \\   ");
