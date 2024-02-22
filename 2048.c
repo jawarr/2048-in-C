38d6534553455ed5f5cf972bf989791ff6f7eb9d
Jacob Warren <jacobiwarren@gmail.com>
2024-02-21 19:53:18 -0600

    added checkBoard func back

--- 2048.c
+++ 2048.c
@@ -406,3 +406,19 @@ void printLogo()
     puts("   |\\________\\ \\_______\\     \\ \\__\\ \\_______\\");
     puts("    \\|_______|\\|_______|      \\|__|\\|_______|\n\n\n");
 }
+
+int checkBoard(int board[][4])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++){
+            if (board[i][j] == board[i][j + 1]){
+                return 1;
+            }
+            else if (board[i][j] == 2048){
+                return 2;
+            }
+        }
+    }
+    return 0;
+}
